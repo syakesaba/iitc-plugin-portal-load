ade8368ea2a1833feae6891ced73c1cb8b57ebce
syakesaba <syakesaba@gmail.com>
2022-03-16 14:45:17 +0900

    isGoogle added

--- portal-link-load.js
+++ portal-link-load.js
@@ -4,8 +4,8 @@
 // @category       Debug
 // @version        0.0.0
 // @namespace      urlload
-// @updateURL      
-// @downloadURL    
+// @updateURL      https://raw.githubusercontent.com/syakesaba/iitc-plugin-portal-load/main/portal-link-load.js
+// @downloadURL    https://raw.githubusercontent.com/syakesaba/iitc-plugin-portal-load/main/portal-link-load.js
 // @description    Open Portal Load Link
 // @include https://intel.ingress.com/intel*
 // @match https://intel.ingress.com/intel*
@@ -14,17 +14,9 @@
 // @grant          none
 // ==/UserScript==
 
-
 function wrapper(plugin_info) {
 // ensure plugin framework is there, even if iitc is not yet loaded
 if(typeof window.plugin !== 'function') window.plugin = function() {};
-
-//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
-//(leaving them in place might break the 'About IITC' page or break update checks)
-plugin_info.buildName = 'iitc';
-plugin_info.dateTimeVersion = '20170108.21732';
-plugin_info.pluginId = 'debug-portal-load';
-//END PLUGIN AUTHORS NOTE
 
 // PLUGIN START ////////////////////////////////////////////////////////
 
@@ -41,16 +33,25 @@ window.plugin.urlload.addLink = function(d) {
     console.warn ('Error: failed to find portal details for guid '+guid+' - failed to show debug data');
     return;
   }
+  var isApple = true;
+  var isGoogle = false;
+  var linkURL = false;
   var data = window.portals[guid].options.data;
-  var lat = data.latE6/1E6;
-  var lng = data.lngE6/1E6;
-  var apn = "com.nianticproject.ingress";
-  var isi = "576505181";
-  var ibi = "com.google.ingress";
-  var ifl = "https://apps.apple.com/app/ingress/id576505181";
-  var ofl = "https://intel.ingress.com/intel?pll=" + lat + "," + lng;
-  var linkURL = "https://link.ingress.com/?link=https://intel.ingress.com/portal/" + guid + "&apn=" + apn + "&isi=" + isi + "&ibi=" + ibi + "&ifl=" + encodeURIComponent(ifl) + "&ofl=" + encodeURIComponent(ofl);
-  $('.linkdetails').append('<aside><a href="' + linkURL + '" title="Display URL Load of the portal">Portal Load</a></aside>');
+  if (isApple) {
+    var lat = data.latE6/1E6;
+    var lng = data.lngE6/1E6;
+    var apn = "com.nianticproject.ingress";
+    var isi = "576505181";
+    var ibi = "com.google.ingress";
+    var ifl = "https://apps.apple.com/app/ingress/id576505181";
+    var ofl = "https://intel.ingress.com/intel?pll=" + lat + "," + lng;
+    linkURL = "https://link.ingress.com/?link=https://intel.ingress.com/portal/" + guid + "&apn=" + apn + "&isi=" + isi + "&ibi=" + ibi + "&ifl=" + encodeURIComponent(ifl) + "&ofl=" + encodeURIComponent(ofl);
+  } else if (isGoogle){
+    linkURL = "";
+  } else {
+    linkURL = "";
+  }
+  $('.linkdetails').append('<aside><a href="' + linkURL + '" target="_blank" title="Display URL Load of the portal">Portal Load</a></aside>');
 }
 
 var setup = function () {
